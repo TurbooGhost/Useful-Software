ca1ef6574a59fe514fa3c8c1d83e79259a9172da
TurbooGhost <mhmd.turboo@gmail.com>
2023-09-08 08:44:07 -0700

    Massive Update

--- My Project/Projects/Todo List App in JavaScript/script.js
+++ My Project/Projects/Todo List App in JavaScript/script.js
@@ -1,106 +1,177 @@
-const taskInput = document.querySelector(".task-input input"),
-filters = document.querySelectorAll(".filters span"),
-clearAll = document.querySelector(".clear-btn"),
-taskBox = document.querySelector(".task-box");
+"use strict";
 
-let editId,
-isEditTask = false,
-todos = JSON.parse(localStorage.getItem("todo-list"));
-
-filters.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector("span.active").classList.remove("active");
-        btn.classList.add("active");
-        showTodo(btn.id);
-    });
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+  document.title = "Come Back ;(";
 });
 
-function showTodo(filter) {
-    let liTag = "";
-    if(todos) {
-        todos.forEach((todo, id) => {
-            let completed = todo.status == "completed" ? "checked" : "";
-            if(filter == todo.status || filter == "all") {
-                liTag += `<li class="task">
-                            <label for="${id}">
-                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                <p class="${completed}">${todo.name}</p>
-                            </label>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="task-menu">
-                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </li>`;
-            }
-        });
-    }
-    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-    let checkTask = taskBox.querySelectorAll(".task");
-    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
-}
-showTodo("all");
-
-function showMenu(selectedTask) {
-    let menuDiv = selectedTask.parentElement.lastElementChild;
-    menuDiv.classList.add("show");
-    document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != selectedTask) {
-            menuDiv.classList.remove("show");
-        }
-    });
-}
-
-function updateStatus(selectedTask) {
-    let taskName = selectedTask.parentElement.lastElementChild;
-    if(selectedTask.checked) {
-        taskName.classList.add("checked");
-        todos[selectedTask.id].status = "completed";
-    } else {
-        taskName.classList.remove("checked");
-        todos[selectedTask.id].status = "pending";
-    }
-    localStorage.setItem("todo-list", JSON.stringify(todos))
-}
-
-function editTask(taskId, textName) {
-    editId = taskId;
-    isEditTask = true;
-    taskInput.value = textName;
-    taskInput.focus();
-    taskInput.classList.add("active");
-}
-
-function deleteTask(deleteId, filter) {
-    isEditTask = false;
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(filter);
-}
-
-clearAll.addEventListener("click", () => {
-    isEditTask = false;
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo()
+window.addEventListener("focus", () => {
+  document.title = docTitle;
 });
 
-taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value.trim();
-    if(e.key == "Enter" && userTask) {
-        if(!isEditTask) {
-            todos = !todos ? [] : todos;
-            let taskInfo = {name: userTask, status: "pending"};
-            todos.push(taskInfo);
+// selectors
+const todoInputs = document.querySelector(".todo-inputs");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todos");
+let deleteAll = document.querySelector(".delete-all");
+
+//event listners
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodos);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+function addTodos(e) {
+  e.preventDefault();
+
+  if (todoInputs.value.trim() === "") {
+    // alert("Task cannot be empty");
+    return;
+  }
+
+  // todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // new todo
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
+  newTodo.innerText = todoInputs.value;
+  newTodo.contentEditable = false;
+  todoDiv.appendChild(newTodo);
+
+  // Checked button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // Edit button
+  const editButton = document.createElement("button");
+  editButton.innerHTML = '<i class="fas fa-edit"></i>';
+  editButton.classList.add("edit-btn");
+  editButton.addEventListener("click", function () {
+    newTodo.contentEditable = true;
+    newTodo.focus();
+  });
+  todoDiv.appendChild(editButton);
+
+  newTodo.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevents adding a new line
+      if (newTodo.innerText.trim() === "") {
+        alert("Task cannot be empty");
+        newTodo.focus();
+        return;
+      }
+      newTodo.contentEditable = false;
+    }
+  });
+
+  // Trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Append list
+  todoList.appendChild(todoDiv);
+  todoInputs.value = "";
+}
+
+function deleteCheck(e) {
+  const items = e.target;
+
+  // Delete todo
+  if (items.closest(".trash-btn")) {
+    const todo = items.closest(".todo");
+    todo.remove();
+  }
+
+  // Mark todo as complete
+  if (items.closest(".complete-btn")) {
+    const todo = items.closest(".todo");
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
         } else {
-            isEditTask = false;
-            todos[editId].name = userTask;
+          todo.style.display = "none";
         }
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo(document.querySelector("span.active").id);
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
     }
+  });
+}
+function getTodos() {
+  //check
+  let todos = [];
+
+  todos.forEach(function (todo) {
+    //todoDiv
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create li
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-item");
+    newTodo.innerText = todo;
+    todoDiv.appendChild(newTodo);
+
+    // checked button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append list
+    todoList.appendChild(todoDiv);
+  });
+}
+
+// Adding a bounce animation on add button click
+todoButton.addEventListener("click", function () {
+  // Trigger the bounce animation
+  this.classList.add("bounce");
+
+  // Remove the bounce class after the animation completes (0.5s)
+  setTimeout(() => {
+    this.classList.remove("bounce");
+  }, 500);
 });
+
+todoInputs.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTodos(event);
+  }
+});
+
+deleteAll.onclick = function () {
+  todoList.innerHTML = ""; // Clear the todos from UI
+  // If you're storing tasks in local storage or any other storage, make sure to clear them too
+  window.localStorage.removeItem("todos"); // Assuming "todos" is the key you're using to store them
+};
